5e9412403460a05c2ff2d07ea4746cb72aa6fa78
Megawats777 <jedsemilla@gmail.com>
2017-11-28 19:47:49 -0500

    Added event listeners to the restart and quit buttons on the game over screen

--- Assets/Source/UiClasses/GameOverUi.cs
+++ Assets/Source/UiClasses/GameOverUi.cs
@@ -1,14 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOverUi : UiBase
 {
+
+    /*--Ui element references--*/
+    public Button restartButton;
+    public Button quitButton;
+
 
     // Use this for initialization
     void Start()
     {
         hideUi();
+
+        restartButton.onClick.AddListener(delegate
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        });
+
+        quitButton.onClick.AddListener(delegate
+        {
+            Application.Quit();
+        });
     }
+
 
 }
